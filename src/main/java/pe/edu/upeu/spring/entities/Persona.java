0a08e710781956d21be8d78ba3d8a9c87b529ac1
JeanPier1 <38229799+JeanPier1@users.noreply.github.com>
2019-05-08 09:25:24 -0500

    entidades2

---
+++ src/main/java/pe/edu/upeu/spring/entities/Persona.java
@@ -0,0 +1,5 @@
+package pe.edu.upeu.spring.entities;
+
+public class Persona {
+
+}
--- src/main/java/pe/edu/upeu/spring/entities/User.java
+++ src/main/java/pe/edu/upeu/spring/entities/User.java
@@ -1,5 +1,20 @@
 package pe.edu.upeu.spring.entities;
 
-public class User {
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Table(name="MAE_USER")
+public class User {
+	
+	@Id
+	private long id;
+	
+	private String usuario;
+	
+	private String passwoord;
+	
+	private Rol rolid;
+	
+	
+	private Persona personaid;
 }
